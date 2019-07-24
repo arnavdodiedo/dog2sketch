445ab2106e474d219db8f59e39957531d8eaa9ed
Arnav Karmarkar <arnav.karmarkar@gmail.com>
2019-07-24 15:10:26 +0530

    Updated download_images.py

--- download_images.py
+++ download_images.py
@@ -3,8 +3,8 @@ from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 
-os.chdir("C:/Users/ARKARMAR/cyclegan/datasets/trainB/")
+os.chdir("C:/Users/ARKARMAR/cyclegan/datasets/trainB/") # download directory
 
-arguments = {"keywords":"multiple dogs","limit":100,"print_urls":False}   # change keywords according to required dataset 
-paths = response.download(arguments)                                      # max limit 100 
+arguments = {"keywords": "multiple dogs", "limit": 100, "print_urls": False}   # change keywords according to required dataset 
+paths = response.download(arguments)                                           # max limit 100 
 print(paths) 
